3fac0ff4ceaeb0f9da5da688847aeafefac9b56a
freyashah52 <79043754+freyashah52@users.noreply.github.com>
2021-02-14 16:49:15 +0530

    v1.0.0 year 2021

---
+++ sccript.js
@@ -0,0 +1,46 @@
+// messagage and messge for that story section 
+
+function Person1()
+{
+    document.getElementById("img").src="img/z1.jpeg";
+    document.getElementById("para").innerHTML="Happy birthday bhai 💯❤have a great birthday and enjoy😊";
+} 
+ 
+function Person2()
+{
+    document.getElementById("img").src="img/z2.jpeg";
+    document.getElementById("para").innerHTML="Happy Birthday Chaman❤️ our friendship was so unexpected😂 hanging out with you is fun Phele jaise rakhadte hai yaarrr🥺";
+} 
+ 
+function Person3()
+{
+    document.getElementById("img").src="img/z3.jpeg";
+    document.getElementById("para").innerHTML="Happy birthday Vardhan ❤❤";
+} 
+ 
+function Person4()
+{
+    document.getElementById("img").src="img/z4.jpeg";
+    document.getElementById("para").innerHTML="Happpiestt birthdayy item❤💋thank you for always being with me🥰ik tu chodu h  ek no  ka bt mere jaaan h 💋💋💋";
+} 
+ 
+function Person5()
+{
+    document.getElementById("img").src="img/z5.jpeg";
+    document.getElementById("para").innerHTML="Happy birthday baby ❤";
+} 
+ 
+function Person6()
+{
+    document.getElementById("img").src="img/z6.jpg";
+    document.getElementById("para").innerHTML="Happy birthday bhand ❤️ jaldi mil apan machaate h 🥳😂";
+} 
+ 
+function Person7()
+{
+    document.getElementById("img").src="img/z7.jpeg";
+    document.getElementById("para").innerHTML="Happy birthday meri jaan  Busy h ajj kal🤧 but thike koi nai meri jaan jo h bas sath rahio hamesha🥰";
+} 
+ 
+
+ 
